285d9cf4a885b3d433b898f95aeed0fc52d7c9c7
rimjhim roy <rimjhim.roy.ch@gmail.com>
2019-09-30 14:58:44 +0200

    update parse_blast.py

--- parse_blast.py
+++ parse_blast.py
@@ -1,12 +1,26 @@
+#!/usr/bin/env python3
 from blast import parse
+import os, sys, argparse
 
-fh = open('test.outfmt6')
+print()
+
+#create variables that can be entered as arguments in command line
+parser=argparse.ArgumentParser(description='This script takes a blast output in -outfmt 6/ -m8 format and parses it based on several parameters')
+parser.add_argument('-file', type=str, metavar='blast_output', required=True, help='REQUIRED: Full path to blast output')
+parser.add_argument('-evalue', type=str, metavar='evalue_cutoff', default='1e-6', help='Evalue cutoff (string) [1e-6]')
+parser.add_argument('-identity', type=float, metavar='identity_cutoff', default=80, help='Minimum identity cutoff (integer) [80]')                               
+parser.add_argument('-length', type=int, metavar='alignment_length', default=80, help='Minimum alignment length cutoff [80]')
+args = parser.parse_args()
+fh = open(args.file)
+outfile=os.path.basename(args.file)+".bastout"
+outf= open(outfile,"w+")
 for blast_record in parse(fh):
-    print('query id: {}'.format(blast_record.qid))
+    header=['querry','subject','length','evalue','qstart','qend','hstart','hend','perc_identity']
+    outf.write('\t'.join(header[0:])+'\n')
     for hit in blast_record.hits:
         for hsp in hit:
-            print('****Alignment****')
-            print('sequence:', hsp.sid)
-            print('length:', hsp.length)
-            print('e value:', hsp.evalue)
+            if hsp.evalue < float(args.evalue) and hsp.pident > args.identity and hsp.length > args.length:
+                records=[hsp.qid,hsp.sid,hsp.length,hsp.evalue,hsp.qstart,hsp.qend,hsp.sstart,hsp.send, hsp.pident]
+                outf.write('\t'.join(map(str,records[0:])) + '\n')
+outf.close()
 fh.close()
