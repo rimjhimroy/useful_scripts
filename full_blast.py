1849ce060e1f7621428e639996964cf0780a4a58
rimjhim roy <rimjhim.roy.ch@gmail.com>
2019-10-01 11:47:01 +0200

    refactoring blast

--- full_blast.py
+++ full_blast.py
@@ -17,8 +17,8 @@ parser.add_argument('-subject', type=str, metavar='blast_subject',
                     required=True, help='REQUIRED: Full path to subject file')
 parser.add_argument('-program', type=str, metavar='blast_program',
                     required=True, help='REQUIRED: blast program to use blastn/megablast')
-parser.add_argument('-blastout', type=str, metavar='blast_output',
-                    required=True, help='REQUIRED: full path to blast output filename to store in -outfmt 6/ -m8 format')
+parser.add_argument('-blastout', type=str, metavar='blast_output_path',
+                    default="./outblast", help='full path to blast output folder to store in -outfmt 6/ -m8 format [./outblast]')
 parser.add_argument('-evalue', type=str, metavar='evalue_cutoff',
                     default='1e-6', help='Evalue cutoff (string) [1e-6]')
 parser.add_argument('-identity', type=float, metavar='identity_cutoff',
@@ -31,21 +31,35 @@ cmd = ('makeblastdb -dbtype nucl -parse_seqids -in '+args.subject)
 p = subprocess.Popen(cmd, shell=True)
 sts = os.waitpid(p.pid, 0)[1]
 
+outname = os.path.basename(args.query)+'_'+os.path.basename(args.subject)
+if not os.path.exists(args.blastout):
+    os.mkdir(args.blastout)
 if args.program == "megablast":
+    outpath = args.blastout+'/'+outname+'.megablast.out'
     cmd = ('megablast -d '+args.subject+' -i' +
-           args.query+' -m 8 -o '+args.blastout)
+           args.query+' -m 8 -o '+outpath)
     p = subprocess.Popen(cmd, shell=True)
     sts = os.waitpid(p.pid, 0)[1]
 elif args.program == "blastn":
+    outpath = args.blastout+'/'+outname+'.blastn.out'
     cmd = ('blastn -db '+args.subject+' -query ' +
-           args.query+' -outfmt 6 -out '+args.blastout)
+           args.query+' -outfmt 6 -out '+outpath)
     p = subprocess.Popen(cmd, shell=True)
     sts = os.waitpid(p.pid, 0)[1]
 
-pickle.dump(args.blastout, open(os.path.dirname(args.blastout)+"/save.p1", "wb"))
-blastout = pickle.load(open(os.path.dirname(args.blastout)+"/save.p1", "rb"))
-fh = open(blastout)
-outfile = os.path.dirname(args.blastout)+'/'+os.path.basename(args.blastout)+".blastout"
+'''
+if not os.path.exists(args.blastout+"/pickle"):
+    os.mkdir(args.blastout+"/pickle")
+pickle_out1 = open(args.blastout+"/pickle/save.p1", "wb")
+pickle.dump(outpath, pickle_out1)
+pickle_out1.close()
+blastout = open(args.blastout+"/pickle/save.p1", "rb")
+fh = pickle.load(blastout)
+'''
+
+fh = open(outpath)
+outfile = args.blastout+'/' + \
+    os.path.basename(outname)+".filtered."+args.program+".out"
 outf = open(outfile, "w+")
 for blast_record in parse(fh):
     header = ['querry', 'subject', 'length', 'evalue',
