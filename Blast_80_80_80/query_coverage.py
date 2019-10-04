d85711d4efae2db2c8b7a164e53f478942c59139
rimjhim roy <rimjhim.choudhury@ips.unibe.ch>
2019-10-04 18:31:35 +0200

    edit query_coverage

--- Blast_80_80_80/query_coverage.py
+++ Blast_80_80_80/query_coverage.py
@@ -325,7 +325,7 @@ parser.add_argument('-query', type=str, metavar='blast_query',
 parser.add_argument('-identity', type=float, metavar='identity_cutoff',
                     default=80, help='Minimum identity cutoff in %% (float) [80]')
 parser.add_argument('-querycov', type=float, metavar='query_coverage_cutoff',
-                    default=20, help='Minimum query coverage cutoff in %% (float) [20]')
+                    default=20, help='Minimum query coverage cutoff in % (float) [20]')
 parser.add_argument('-hitlength', type=int, metavar='hit_length',
                     default=80, help='Minimum hit length cutoff in bps [80]')
 args = parser.parse_args()
